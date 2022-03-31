2f117b4f2843268019e9f1d193f4742df60b408e
ASUS <Yehor Kholodiuk>
2022-03-31 17:29:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Routes,Route,Link} from 'react-router-dom'
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        return (
+            <div className="wrapper">
+                <h1>Application</h1>
+
+                    <Routes>
+                        <Route path="/dashboard">
+                            <Dashboard />
+                        </Route>
+                        <Route path="/preferences">
+                            <Preferences />
+                        </Route>
+                    </Routes>
+            </div>
+        );
+    }
+
+
+    export default App;
